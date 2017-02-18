377224302f25980892ba19ef1fc236ecfffa1021
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-18 23:11:47 +0000

    Replaced the list comprehension generating boids_y_velocities with a library function (np.random.uniform)

--- boids/boids.py
+++ boids/boids.py
@@ -15,7 +15,7 @@ boid_count = 50
 boids_x = np.random.uniform(-450, 50.0, boid_count)
 boids_y = np.random.uniform(300.0, 600.0, boid_count)
 boid_x_velocities = np.random.uniform(0, 10.0, boid_count)
-boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(boid_count)]
+boid_y_velocities = np.random.uniform(-20.0, 20.0, boid_count)
 boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
 
