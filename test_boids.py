f4a8c4bbe7738b09e3febdaf0447bf3bcffc6934
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-18 22:46:31 +0000

    The given (bad) code

---
+++ test_boids.py
@@ -0,0 +1,13 @@
+from boids import update_boids
+from nose.tools import assert_almost_equal
+import os
+import yaml
+
+def test_bad_boids_regression():
+    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    boid_data=regression_data["before"]
+    update_boids(boid_data)
+    for after,before in zip(regression_data["after"],boid_data):
+        for after_value,before_value in zip(after,before): 
+            assert_almost_equal(after_value,before_value,delta=0.01)
+	
