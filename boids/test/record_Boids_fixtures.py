f06291de0f3ea20275c84d6bd86917d37f19c4de
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-20 20:25:41 +0000

    Wrote code to generate a fixture fo the fly_away_from_nearby_boids function, and generate said fixture

--- boids/test/record_Boids_fixtures.py
+++ boids/test/record_Boids_fixtures.py
@@ -1,5 +1,5 @@
 import yaml
-from boids.boids import Boids
+from boids import Boids
 from copy import deepcopy
 import numpy as np
 
@@ -10,5 +10,18 @@ boids.fly_towards_the_middle(boids.boids,move_to_middle)
 after= boids.boids
 fixture={"before":np.array(before).tolist(),"after":np.array(after).tolist(), "move_to_middle_strength":move_to_middle}
 fixture_file=open("fixture_fly_towards_the_middle.yml",'w')
+fixture_file.write(yaml.dump(fixture))
+fixture_file.close()
+
+boids = Boids()
+before=deepcopy(boids.boids)
+boids.separation(before[0],before[1])
+alert_distance = 100
+boids.fly_away_from_nearby_boids(boids.boids,boids.x_separation, boids.y_separation, boids.separation_distance_squared,alert_distance)
+after= boids.boids
+fixture={"before":np.array(before).tolist(),
+         "after":np.array(after).tolist(),
+         "alert_distance":alert_distance}
+fixture_file=open("fixture_fly_away_from_nearby_boids.yml",'w')
 fixture_file.write(yaml.dump(fixture))
 fixture_file.close()
