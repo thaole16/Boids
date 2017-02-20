471b4a5f0a774594b69dfe5625727ee4d3de7c54
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-20 21:20:15 +0000

    Addeed setup.py file (preliminary things)

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Boids",
+
+    version="0.x.x",
+
+    description="Boid flocking",
+    long_description=open('README.md').read(),
+
+    url='https://github.com/thaole16/TOADDURLHERE',
+
+    author='MPHYG001, Thao Le',
+    author_email='thao.le.16@ucl.ac.uk',
+
+    packages=find_packages(exclude=['*test','docs']),
+
+    #entry_points={'console_scripts': ['boids=boids.command:process']},
+
+    install_requires=['argparse','numpy','matplotlib','nose.tools','os','yaml','mock'],
+
+    license="MIT",
+
+    classifiers=[
+        'License :: MIT License',
+        'Intended Audience :: MPHYG001 Markers',
+        'Programming Language :: Python :: 2.7',
+
+    ]
+)
