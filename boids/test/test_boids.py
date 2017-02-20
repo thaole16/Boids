b280aa112fd356ad39a76f2af339bd52d7a772c7
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-20 17:49:10 +0000

    Basic test for the Boids __init__ (checking the sizes of the different arrays)

--- boids/test/test_boids.py
+++ boids/test/test_boids.py
@@ -1,5 +1,5 @@
-from boids import update_boids
-from nose.tools import assert_almost_equal
+from boids import Boids, update_boids
+from nose.tools import assert_almost_equal,assert_equals
 import os
 import yaml
 import numpy as np
@@ -12,3 +12,9 @@ def test_bad_boids_regression():
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
 	
+def test_Boids():
+    boidobject = Boids()
+    assert_equals(boidobject.boids_x.size, 50)
+    assert_equals(boidobject.boids_y.size, 50)
+    assert_equals(boidobject.boid_x_velocities.size, 50)
+    assert_equals(boidobject.boid_y_velocities.size, 50)
