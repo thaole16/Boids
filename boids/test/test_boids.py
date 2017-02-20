835b7c06401b2de94ca834f4529137697174889f
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-20 18:07:49 +0000

    Updated the test file to reflect that the functions are now inside the Boids class

--- boids/test/test_boids.py
+++ boids/test/test_boids.py
@@ -1,4 +1,4 @@
-from boids import Boids, update_boids
+from boids import Boids
 from nose.tools import assert_almost_equal,assert_equals, assert_raises
 import os
 import yaml
@@ -7,7 +7,7 @@ import numpy as np
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
     boid_data=np.array(regression_data["before"])
-    update_boids(boid_data)
+    Boids().update_boids(boid_data)
     for after,before in zip(regression_data["after"],boid_data):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
