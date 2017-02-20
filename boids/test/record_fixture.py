86ce139152c6787cc77e9c27825f8d17be4e3319
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-20 17:18:52 +0000

    Updated the fixture for the regression test since now the interpretation of the Boid model has changed (to instantaneous velocity matching)

--- boids/test/record_fixture.py
+++ boids/test/record_fixture.py
@@ -1,9 +1,9 @@
 import yaml
-import boids
+import boids.boids.boids
 from copy import deepcopy
-before=deepcopy(boids.boids)
-boids.update_boids(boids.boids)
-after=boids.boids
+before=deepcopy(boids.boids.boids)
+boids.boids.update_boids(boids.boids.boids)
+after= boids.boids.boids
 fixture={"before":before,"after":after}
 fixture_file=open("fixture.yml",'w')
 fixture_file.write(yaml.dump(fixture))
