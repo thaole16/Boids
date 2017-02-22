e8cd7ee5455f7cd83230ee8d202d0a1b2eb3ea67
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-22 12:29:06 +0000

    Updated test_bad_boids to reflect change in input parameters to update boids

--- boids/test/test_bad_boids.py
+++ boids/test/test_bad_boids.py
@@ -7,7 +7,8 @@ import numpy as np
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml')))
     boid_data=np.array(regression_data["before"])
-    Boids().update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
+    Boids().update_boids((boid_data[0:2],boid_data[2:4]))
+    after_data = np.array(regression_data["after"])
+    for after,before in zip(((after_data[0:2],after_data[2:4])),boid_data):
         for after_value,before_value in zip(after,before):
             assert_almost_equal(after_value,before_value,delta=0.01)
