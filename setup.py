57a0ed32f7f82c44c9ac56bcdcb0eab1288ac791
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-22 17:58:50 +0000

    Updated version number to 1.0.0 git add setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Boids",
 
-    version="0.1.1",
+    version="1.0.0",
 
     description="Boid flocking",
     long_description=open('README.md').read(),
