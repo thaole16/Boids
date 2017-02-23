70d8096bd23a42c9b00114f2daef2277f3b9f185
thaole16 <thao.le.16@ucl.ac.uk>
2017-02-23 11:08:19 +0000

    Updated the regression fixture and test code

--- boids/test/test_bad_boids.py
+++ boids/test/test_bad_boids.py
@@ -7,8 +7,8 @@ import numpy as np
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml')))
     boid_data=np.array(regression_data["before"])
-    Boids().update_boids((boid_data[0:2],boid_data[2:4]))
+    Boids().update_boids(boid_data)
     after_data = np.array(regression_data["after"])
-    for after,before in zip(((after_data[0:2],after_data[2:4])),(boid_data[0:2],boid_data[2:4])):
+    for after,before in zip(after_data,boid_data):
         for after_value,before_value in zip(after,before):
             np.testing.assert_almost_equal(after_value,before_value)
